841f99b4f5dd7c55054fc5928010786de911dc53
Avyukta Dinesh <avyuktadinesh912@gmail.com>
2024-06-19 21:29:53 -0600

    Added a sign in feature

--- backend/db.py
+++ backend/db.py
@@ -8,10 +8,22 @@ try:
     usersCollection = db['users']
     
     def signUp(userName,email):
+        user = usersCollection.find({"username":userName})
+        a = []
+
+        for i in user:
+            a.append[i]
+
         userInfo = {"username":userName,"email":email}
         insert_user = usersCollection.insert_one(userInfo)
-        if insert_user:
-            return True
+        
+        if len(a) > 0:
+            if insert_user:
+                return True
+            else:
+                return False
+        else:
+            return False
 
 except Exception as e:
     print(f"There was an error {e}")
